7aa92d1f52befda8c2f37b30a5d14891d2f16056
chan <chanunlang@gmail.com>
2017-08-14 15:56:29 +0800

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { } from './components';
+
+import { } from './directives';
+
+import { } from './services';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
